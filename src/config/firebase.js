8c1b93ef69a877d9adfea6ff812fea0914a161ad
Gaius G D'cruz <gaingaius123@gmail.com>
2020-09-02 07:16:58 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,14 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: "AIzaSyAAqozkdhRucPwBDleNd6ZgxWEpxU1spuI",
+  authDomain: "project-dsc-jcmcsiit.firebaseapp.com",
+  databaseURL: "https://project-dsc-jcmcsiit.firebaseio.com",
+  projectId: "project-dsc-jcmcsiit",
+  storageBucket: "project-dsc-jcmcsiit.appspot.com",
+  messagingSenderId: "119695472216",
+  appId: "1:119695472216:web:639757c6607ce137edbc2d",
+  measurementId: "G-PPKY9GN4N4"
 };
 
 firebase.initializeApp(firebaseConfig);
